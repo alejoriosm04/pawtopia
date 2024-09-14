0ae4a4b099839491afb99008d3426a5acacba40e
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2024-09-14 01:45:58 -0500

    feat: Add Pet translations for English and Spanish

---
+++ resources/lang/es/Pet.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'pets_title' => 'Mascotas - Pawtopia',
+    'pets_subtitle' => 'Lista de mascotas',
+    'pet_info_title' => ':name - Pawtopia',
+    'pet_info_subtitle' => ':name - Información de la mascota',
+    'create_pet_title' => 'Crear mascota',
+    'pet_created_title' => 'Mascota creada',
+    'pet_created_message' => 'Mascota creada exitosamente',
+];
