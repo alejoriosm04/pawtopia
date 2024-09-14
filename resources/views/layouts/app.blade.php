c183887cb19706d9f1d045b8c1795e3bde1f7a40
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2024-09-13 23:26:09 -0500

    refactor: Refactor color scheme in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,20 +12,20 @@
   <!-- header -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-4">
     <div class="container">
-      <a class="navbar-brand" href="{{ route('home.index') }}" style="color: #F04A23;">Pawtopia</a>
+      <a class="navbar-brand" href="{{ route('home.index') }}" style="color: #DB4D20;">Pawtopia</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav ms-auto">
-          <a class="nav-link active" href="{{ route('home.index') }}" style="color: #F04A23;">Home</a>
+          <a class="nav-link active" href="{{ route('home.index') }}" style="color: #DB4D20;">Home</a>
         </div>
       </div>
     </div>
   </nav>
 
-  <header class="masthead text-white text-center py-4" style="background-color: #F04A23;">
+  <header class="masthead text-white text-center py-4" style="background-color: #DB4D20;">
     <div class="container d-flex align-items-center flex-column">
       <h2>@yield('subtitle', 'Pawtopia')</h2>
     </div>
