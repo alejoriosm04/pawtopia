f7eeffda2ab2e30a2633d1845a950f90eddb3c40
linasofi13 <lina.ballesteros13@gmail.com>
2024-09-14 01:22:03 -0500

    feat: Add Product model with attributes and validation

---
+++ app/Models/Product.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
+class Product extends Model
+{
+    /**
+     * PRODUCT ATTRIBUTES
+     * $this->attributes['id'] - int - contains the product primary key (id)
+     * $this->attributes['name'] - string - contains the product name
+     * $this->attributes['description'] - string - contains the product description
+     * $this->attributes['category'] - string - contains the product category
+     * $this->attributes['image'] - string - contains the product image
+     * $this->attributes['price'] - int - contains the product price
+     * $this->attributes['created_at'] - timestamp - contains the product creation date
+     * $this->attributes['updated_at'] - timestamp - contains the product update date
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image',
+    ];
+    public function getId(): int
+    {
+        return $this->attributes['id'];
+    }
+
+    public function getName(): string
+    {
+        return $this->attributes['name'];
+    }
+
+    public function setName(string $name): void
+    {
+        $this->attributes['name'] = $name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->attributes['description'];
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->attributes['description'] = $description;
+    }
+
+    public function getImage(): string
+    {
+        return $this->attributes['image'];
+    }
+
+    public function setImage(string $image): void
+    {
+        $this->attributes['image'] = $image;
+    }
+
+    public function getPrice(): int
+    {
+        return $this->attributes['price'];
+    }
+
+    public function setPrice(int $price): void
+    {
+        $this->attributes['price'] = $price;
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->attributes['created_at'];
+    }
+
+    public function setCreatedAt($createdAt): void
+    {
+        $this->attributes['created_at'] = $createdAt;
+    }
+
+    public function getUpdatedAt(): string
+    {
+        return $this->attributes['updated_at'];
+    }
+
+    public function setUpdatedAt($updatedAt): void
+    {
+        $this->attributes['updated_at'] = $updatedAt;
+    }
+    /* public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+*/
+
+    public static function validate(Request $request): void
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric|gt:0',
+            'image' => 'image',
+        ]);
+    }
+}
