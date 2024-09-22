c5ac316abbee55c15af90c976932dfd53afe9934
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2024-09-22 13:11:19 -0500

    feat: Add foreign key constraint for user_id in pets table migration

--- database/migrations/2024_09_22_180522_create_items_table.php
+++ database/migrations/2024_09_22_180522_create_items_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->foreign('order_id')->references('id')->on('orders');
             $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products');
+            $table->foreignId('pet_id')->constrained('pets')->onDelete('cascade');
             $table->timestamps();
         });
     }
