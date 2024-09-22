45f68373e3a3623ca1849be927864e1bf9cf15b5
linasofi13 <lina.ballesteros13@gmail.com>
2024-09-22 11:55:59 -0500

    feat: Add frontend details

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,11 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-
-    <!-- Custom CSS -->
     <link href="{{ asset('/css/admin.css') }}" rel="stylesheet" />
 
-    <!-- Google Fonts - Lexend -->
     <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <title>@yield('title', __('admin/Layout.title'))</title>
