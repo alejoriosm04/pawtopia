328ebb8930ceb325343ca647027b04ac20eca8e5
Juanstevan1 <juanestebangarciagalvis@gmail.com>
2024-09-23 01:19:26 -0500

    fix: car

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
 
 class Product extends Model
 {
