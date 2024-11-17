83462e79ae05761f7a5e128c8b70974a76bb70df
Alejandro Ríos Muñoz <ariosm@eafit.edu.co>
2024-11-16 19:26:48 -0500

    Update AdminProductController.php

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -12,7 +12,6 @@ use App\Models\Species;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use App\Interfaces\ImageStorage;
 use Exception;
 
 class AdminProductController extends Controller
